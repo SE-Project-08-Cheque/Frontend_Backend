13a3803d678f8489ec61de84ece1b9c5de11a2ad
nipdep <nipun1deelaka@gmail.com>
2021-01-15 08:50:59 +0530

    Add: database.js, db-function.js > configure eternal db

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const fs = require('fs');
+
+
+module.exports = mysql.createPool({
+    user: 'umaim64n3koovo8v',
+    host: 'bq7l4yjndinfmy4tdyaa-mysql.services.clever-cloud.com',
+    database: 'bq7l4yjndinfmy4tdyaa',
+    port: 3306,
+    password: 'ADZS2xBRWVwJzo84w7dW',
+})
+
+
+
+
+
